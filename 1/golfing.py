955965e757b1a1388c28fd1dfa34efa30eb34416
Kevin Brandstatter <kevin@LittleStorm.(none)>
2012-11-05 15:38:14 -0600

    golfer done, added command line options for configurability

--- 1/golfing.py
+++ 1/golfing.py
@@ -2,6 +2,7 @@
 from threading import Semaphore, Lock, Thread
 import random
 import time
+import sys
 
 rng = random.Random()
 rng.seed(100)
@@ -19,16 +20,16 @@ def golfer(balls, n):
 	global balls_on_field
 	while True:
 		print("Golfer ", n , " calling for bucket")
+		stash_access.acquire()
 		if (stash < balls):
 			cart_collect.release();
 			stash_full.acquire()
-		stash_access.acquire()
 		stash -= balls
 		print("Golfer " , n ," got " , balls ," balls; Stash = ", stash)
 		stash_access.release()
 		for b in range(1,balls):
 			ball_growth.acquire()
-			print("Golfer ", n , " hit ball ", b)
+			print("Golfer ", n , " hit ball ", b )
 			balls_on_field += 1
 			ball_growth.release()
 			time.sleep(rng.random())
@@ -40,22 +41,32 @@ def cart():
 		cart_collect.acquire()
 		print("###################################")
 		print("Cart entering field")
-		stash_access.acquire()
+		ball_growth.acquire()
 		tmp = balls_on_field
 		stash += balls_on_field
+		balls_on_field = 0
 		time.sleep(rng.random())
+		ball_growth.release()
 		print("Cart dont, gathered ", tmp," balls; Stash = ", stash)
 		print("###################################")
-		stash_access.release()
 		stash_full.release()
 
 if __name__ == '__main__':
 	print("Program started")
-	stash = 100
+	ngolfers = 0 
+	bucketsize =0 
+	stash =0
+	try:
+		ngolfers = int(sys.argv[1])
+		bucketsize = int(sys.argv[2])
+		stash = int(sys.argv[3])
+	except (ValueError, IndexError):
+		print("golfers.py <num_golfers> <bucket_size> <stash_size>")
+		sys.exit()
 	ct = Thread(target=cart)
 	ct.start()
-	for n in range(1,5):
-		t = Thread(target=golfer, args=[20,n])
+	for n in range(1,ngolfers):
+		t = Thread(target=golfer, args=[bucketsize,n])
 		t.start() 
 	
 
